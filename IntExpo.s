2a57d2d64547e42b0113405b3150ac66b6b56e1c
Aaron Lewis <alewis2@umbc.edu>
2018-11-28 12:53:44 -0500

    Int exponents done

---
+++ IntExpo.s
@@ -0,0 +1,38 @@
+@variables
+.equ SWI_Exit,  0x11
+ e:
+    .float 2.718
+ zero:
+    .float 0
+    
+_start:
+    LDR  r0, 2 @set base
+    MOV  r1, #3 @set exponent
+
+    MOV  r2, #1 @set power result to 0
+    LDR  r3, zero @set mul product to 0
+
+    LDR  r6, zero @expLoop iteration
+    LDR  r7, zero @mulLoop iteration
+
+expLoop: @for(int i = r6, i < r1, i++)
+    LDR  r3, zero @reset mul product
+    LDR  r7, zero @reset mul iterator
+
+mulLoop: @for(int j = r7, j < r2, j++)
+    ADD  r3, r0 @add base to mulproduct
+    ADD  r7, #1 @j++
+    CMP  r7, r2 @i < r2?
+    BLT  mulLoop
+
+    MOV  r2, r3 @move new product to final result holder
+    ADD  r6, #1 @i++
+    CMP  r6, r1 @i < r1?
+    BLT  expLoop
+
+
+
+
+
+end:
+    swi  SWI_Exit
